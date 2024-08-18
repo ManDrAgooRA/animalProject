7ca4af080410f615df28217de0974d9f24efdae9
eugene <egorobey@lytvynov-production.com>
2024-08-18 16:02:10 +0300

    feature: add header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,7 +1,33 @@
+'use client'
+
 import React from 'react'
 
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import clsx from 'clsx'
+
+import { HEADER_LINKS } from './links'
+
 const Header = () => {
-  return <div>Header</div>
+  const pathname = usePathname()
+  return (
+    <div className='flex justify-center bg-blue-600 py-2 align-middle text-neutral-50'>
+      {HEADER_LINKS.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx('mr-1 rounded p-2', {
+              'bg-sky-100 text-blue-600': pathname === link.href,
+            })}
+          >
+            {link.name}
+          </Link>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Header
---
+++ app/components/Header/links.ts
@@ -0,0 +1,14 @@
+export const HEADER_LINKS = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'Cats',
+    href: '/cats',
+  },
+  {
+    name: 'Dogs',
+    href: '/dogs',
+  },
+]
